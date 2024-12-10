4b95500832e02f5dd79f933176a69c11235ce5bf
Tom Daley <tom@powerdaley.com>
2024-12-09 20:56:59 -0600

    Update translator.py

--- translator.py
+++ translator.py
@@ -36,7 +36,7 @@ def process_messages(file_path):
                 # Google Translate
                 parts = line.split('\t')
                 if len(parts) < 3:
-                    data.append([row_num, date, "", " ".join(parts))
+                    data.append([row_num, date, "", " ".join(parts)])
                     continue
                 translated_text = Translator().translate(line.split('\t')[2]).text
                 data.append([row_num, date, translated_text, line])
